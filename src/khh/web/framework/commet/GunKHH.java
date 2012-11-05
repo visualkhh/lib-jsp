a36f32bcd16306287275c3222a901167d8597877
visualkhh <visualkhh@gmail.com>
2012-11-05 01:19:18 +0000

    f

---
+++ src/khh/web/framework/commet/GunKHH.java
@@ -0,0 +1,22 @@
+package khh.web.framework.commet;
+
+import khh.web.framework.commet.longpolling.Gun;
+
+public class GunKHH extends Gun {
+	int i=0;
+	@Override
+	public Object trigger() throws Exception {
+		Object o = null;
+		while(true){
+			try {
+				Thread.sleep(getInterval());
+				o="khh"+i+"       cometsize "+getCometEventList().size();
+				i++;
+				break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return o;
+	}
+}
